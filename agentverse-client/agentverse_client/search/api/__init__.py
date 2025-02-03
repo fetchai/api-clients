f608bbb7e3049af675c5f767ab53b81cb19d3c11
fetchbot <fetchbot@fetch.ai>
2025-02-03 15:19:06 +0000

    Adding or updating docs and client for search_api

--- agentverse-client/agentverse_client/search/api/__init__.py
+++ agentverse-client/agentverse_client/search/api/__init__.py
@@ -1,9 +1,5 @@
 # flake8: noqa
 
 # import apis into api package
-from agentverse_client.search.api.agents_api import AgentsApi
-from agentverse_client.search.api.analytics_api import AnalyticsApi
-from agentverse_client.search.api.functions_api import FunctionsApi
-from agentverse_client.search.api.public_api import PublicApi
 from agentverse_client.search.api.search_api import SearchApi
 
