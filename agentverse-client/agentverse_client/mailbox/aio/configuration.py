67ed59d51beb56366b7128386e5470844b0e2894
Alejandro-Morales <alejandro.morales.mad@gmail.com>
2025-06-06 13:02:05 -0600

    Revert "Adding or updating docs and client for mailbox"
    This reverts commit f5e55f3a2df5725bb06def644f54a03bf9a46ca6.

--- agentverse-client/agentverse_client/mailbox/aio/configuration.py
+++ agentverse-client/agentverse_client/mailbox/aio/configuration.py
@@ -229,7 +229,7 @@ class Configuration:
         self.logger = {}
         """Logging Settings
         """
-        self.logger["package_logger"] = logging.getLogger("mailbox.aio")
+        self.logger["package_logger"] = logging.getLogger("agentverse_client.mailbox.aio")
         self.logger["urllib3_logger"] = logging.getLogger("urllib3")
         self.logger_format = '%(asctime)s %(levelname)s %(message)s'
         """Log format
--- agentverse-client/mailbox/api/__init__.py
+++
@@ -1,5 +0,0 @@
-# flake8: noqa
-
-# import apis into api package
-from mailbox.api.mailbox_api import MailboxApi
-
