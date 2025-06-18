ca26d3cbafe089cbf35d2d282f2b67bcbf8833dd
fetchbot <fetchbot@fetch.ai>
2025-06-18 14:02:45 +0000

    Adding or updating docs and client for mailbox

--- agentverse-client/agentverse_client/mailbox/__init__.py
+++ agentverse-client/agentverse_client/mailbox/__init__.py
@@ -17,7 +17,14 @@
 __version__ = "1.0.0"
 
 # import apis into sdk package
+from agentverse_client.mailbox.api.agents_api import AgentsApi
+from agentverse_client.mailbox.api.api_keys_api import ApiKeysApi
+from agentverse_client.mailbox.api.auth_api import AuthApi
+from agentverse_client.mailbox.api.exchange_api import ExchangeApi
+from agentverse_client.mailbox.api.feedback_api import FeedbackApi
 from agentverse_client.mailbox.api.mailbox_api import MailboxApi
+from agentverse_client.mailbox.api.profile_api import ProfileApi
+from agentverse_client.mailbox.api.users_api import UsersApi
 
 # import ApiClient
 from agentverse_client.mailbox.api_response import ApiResponse
