51a149a3cecd3e89b97ee9a6a75d256515e18429
fetchbot <fetchbot@fetch.ai>
2025-06-27 09:09:19 +0000

    Adding or updating docs and client for search_api

--- agentverse-client/agentverse_client/search/aio/test/test_public_api.py
+++ agentverse-client/agentverse_client/search/aio/test/test_public_api.py
@@ -110,13 +110,6 @@ class TestPublicApi(unittest.IsolatedAsyncioTestCase):
         """
         pass
 
-    async def test_trigger_seo(self) -> None:
-        """Test case for trigger_seo
-
-        Verifier Feedback Request
-        """
-        pass
-
 
 if __name__ == '__main__':
     unittest.main()
